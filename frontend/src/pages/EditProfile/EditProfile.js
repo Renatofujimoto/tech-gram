d24ba419078b7507cde692a576c2d9e105127a81
Renato Fujimoto <renatofujimoto2@gmail.com>
2023-06-16 23:06:12 -0300

    updating user

--- frontend/src/pages/EditProfile/EditProfile.js
+++ frontend/src/pages/EditProfile/EditProfile.js
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 // Redux
-import { profile, resetMessage } from '../../slices/userSlice'
+import { profile, resetMessage, updateProfile } from '../../slices/userSlice'
 
 // Componets
 import Message from '../../components/Message'
@@ -38,8 +38,40 @@ const EditProfile = () => {
     }
   }, [user])
 
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
     e.preventDefault()
+
+    // Gather user data from start
+    const userData = {
+      name
+    }
+
+    if (profileImage) {
+      userData.profileImage = profileImage
+    }
+
+    if (bio) {
+      userData.bio = bio
+    }
+
+    if (password) {
+      userData.password = password
+    }
+
+    // build form data
+    const formData = new FormData()
+
+    const userFormData = Object.keys(userData).forEach(key =>
+      formData.append(key, userData[key])
+    )
+
+    formData.append('user', userFormData)
+
+    await dispacth(updateProfile(formData))
+
+    setTimeout(() => {
+      dispacth(resetMessage())
+    }, 2000)
   }
 
   const handleFile = e => {
@@ -81,7 +113,6 @@ const EditProfile = () => {
           <span>Imagem do Perfil:</span>
           <input type="file" onChange={handleFile} />
         </label>
-
         <label>
           <span>Bio:</span>
           <input
@@ -91,7 +122,6 @@ const EditProfile = () => {
             value={bio || ''}
           />
         </label>
-
         <label>
           <span>Quer alterar sua senha?</span>
           <input
@@ -101,7 +131,10 @@ const EditProfile = () => {
             value={password || ''}
           />
         </label>
-        <input type="submit" value="Atualizar" />
+        {!loading && <input type="submit" value="Atualizar" />}
+        {loading && <input type="submit" disabled value="Aguarde..." />}
+        {error && <Message msg={error} type="error" />}
+        {message && <Message msg={message} type="sucess" />}
       </form>
     </div>
   )
